6395d3e9dabd7cc148aac9b75c675d7441fef5bf
Conor <conor@conorfox.com>
2021-02-25 15:31:48 +0000

    Initial Vertex

---
+++ lab3/vertex.py
@@ -0,0 +1,11 @@
+"""Class to represent a Vertex as part of a Graph."""
+
+class Vertex:
+    def __init__(self, label):
+        self._label = label
+
+    def __str__(self):
+        return str(label)
+
+    def element(self):
+        return self._label
