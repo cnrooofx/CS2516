8fe7fa9e7107367db59696fac12276b56326097b
Conor Fox <conor@conorfox.com>
2021-03-18 17:23:37 +0000

    Move classes into one file

--- lab3/graph.py
+++ lab3/graph.py
@@ -1,10 +1,58 @@
-"""Class to represent an Undirected Graph."""
+"""Classes that make up an Undirected Graph ADT."""
 
-from edge import Edge
-from vertex import Vertex
+
+class Vertex:
+    """Class to represent a Vertex as part of a Graph."""
+
+    def __init__(self, element):
+        self._element = element
+
+    def __str__(self):
+        return str(self._element)
+
+    def __lt__(self, other):
+        return self._element < other.element()
+
+    def element(self):
+        return self._element
+
+
+class Edge:
+    """Class to represent an Edge between two vertices in a Graph."""
+
+    def __init__(self, v1, v2, label):
+        self._label = label
+        self._v1 = v1
+        self._v2 = v2
+
+    def __str__(self):
+        return "({} -- {} : {})".format(self._v1, self._v2, self._label)
+
+    def element(self):
+        return self._label
+
+    def vertices(self):
+        """Return the pair of vertices in the edge."""
+        return (self._v1, self._v2)
+
+    def start(self):
+        """Return the first vertex in the ordered pair."""
+        return self._v1
+
+    def end(self):
+        """Return the second vertex in the ordered pair."""
+        return self._v2
+
+    def opposite(self, vertex):
+        if vertex == self._v1:
+            return self._v2
+        elif vertex == self._v2:
+            return self._v1
+        return None
 
 
 class Graph:
+    """Class to represent an Undirected Graph."""
     def __init__(self):
         """Initialise a new Graph."""
         self._adj_map = {}
