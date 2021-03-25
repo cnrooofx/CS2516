7c79ec57401edec986208809d417fc883a33f39c
Conor Fox <conor@conorfox.com>
2021-03-25 18:00:43 +0000

    Fix index errors in bubble up & down

--- lab1/binaryheap.py
+++ lab1/binaryheap.py
@@ -29,10 +29,10 @@ class MinHeap:
     def __init__(self):
         self._body = []
         self._size = 0
-    
+
     def size(self):
         return self._size
-    
+
     def add(self, key, item):
         element = Element(key, item)
         self._body.append(element)
@@ -43,7 +43,7 @@ class MinHeap:
         if self._size == 0:
             return None
         return self._body[0]
-    
+
     def remove_min(self):
         min_value = self.get_min()
         if min_value:
@@ -56,7 +56,7 @@ class MinHeap:
 
     def _bubbleup(self, i):
         parent = (i - 1) // 2
-        if self._body[i] < self._body[parent]:
+        if parent >= 0 and self._body[i] < self._body[parent]:
             self._swap(i, parent)
             self._bubbleup(parent)
 
@@ -66,9 +66,9 @@ class MinHeap:
         minchild = left
         if right < self._size and self._body[right] < self._body[left]:
             minchild = right
-        if self._body[i] > self._body[minchild]:
+        if left < self._size and self._body[i] > self._body[minchild]:
             self._swap(i, minchild)
-            self._bubbledown(i)
+            self._bubbledown(minchild)
 
     def _swap(self, i, j):
         self._body[i], self._body[j] = self._body[j], self._body[i]
