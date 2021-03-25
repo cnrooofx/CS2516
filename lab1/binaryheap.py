6c5ec993574d7c69dc4751387b2fa35750f1dab0
Conor Fox <conor@conorfox.com>
2021-03-25 16:34:53 +0000

    Add to binary heap

--- lab1/binaryheap.py
+++ lab1/binaryheap.py
@@ -5,18 +5,44 @@ class Element:
         self._element = item
         self._position = 0
     
-    def _left(self):
-        return 2 * self._position + 1
+    def __str__(self):
+        return "({}, {})".format(self._key, self._element)
+    
+    def __eq__(self, other):
+        return self._key == other.key
 
-    def _right(self):
-        return 2 * self._position + 2
+    def __lt__(self, other):
+        return self._key < other.key
 
-    def _parent(self):
-        return (self._position - 1) // 2
+    def __gt__(self, other):
+        return self._key > other.key
 
+    @property
+    def key(self):
+        return self._key
+
+    @property
+    def value(self):
+        return self._value
 class BinaryHeap:
     def __init__(self):
         self._body = []
+        self._size = 0
+    
+    def size(self):
+        return self._size
     
     def add(self, key, item):
         element = Element(key, item)
+        self._body.append(element)
+        self._bubbleup(self._size)
+        self._size += 1
+
+    def _bubbleup(self, i):
+        parent = (i - 1) // 2
+        if self._body[i] < self._body[parent]:
+            self._swap(i, parent)
+            self._bubbleup(parent)
+
+    def _swap(self, i, j):
+        self._body[i], self._body[j] = self._body[j], self._body[i]
